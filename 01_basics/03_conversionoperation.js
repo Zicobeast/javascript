a50259dde1b5c9f92dcd026a76730ccb50a9bc5c
Zicobeast <147521809+Zicobeast@users.noreply.github.com>
2024-09-10 10:18:42 +0000

    conversion

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,40 @@
+
+// let score = "33"            // "33"       - > 33
+// let score = "33abc"         // "33abc"     -> Nan
+// let score = null            // "Null"      -> 0
+// let score = undefined       // "undefined" ->Nan 
+
+
+// console.log(typeof score);
+
+// let valueNumber = Number(score)
+
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+/* if variable = 1   -> true
+   variable = 0     -> False
+   variable = ""   -> false
+   variable ="bishal" -> true
+*/
+
+
+// let someNumber = 33
+
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+// **************** Operations *******************
+
+
+let value = 3
+let negValue = -value
+console.log(negValue);
